1a45a40163d461981f6d815c74f57136a527b649
fshoukat111 <fshoukat111@gmail.com>
2022-01-21 10:12:40 +0500

    'code-clean'

--- src/app/controllers/article.controller.js
+++ src/app/controllers/article.controller.js
@@ -1,85 +1,29 @@
 const Article = require('../models/article.model');
-const cors = require('cors');
-const corsOptions = {
-	origin: "http://localhost:4200/article"
+
+exports.createArticle = async (req, res) => {
+    const article = await Article.create(req.body);
+    res.status(200).json({
+        success: true,
+        article
+    })
 };
 
-exports.createArticle = (req, res) => {
-    // if (!req.body.title) {
-    //     res.status(400).send({ message: "Content can not be empty!" });
-    //     return;
-    // }
-
-    const article = new Article({
-        title: req.body.title,
-        articleDescription: req.body.articleDescription,
-        // published: req.body.published ? req.body.published : false,
-    });
-
-    article.save().then((data) => {
-        console.log(data);
-        res.send(data);
-    }).catch((err) => {
-        res.status(500).send({
-            message:
-                err.message || "Some error occurred while creating the Article."
-        });
-    });
+exports.getArticles = async (req, res) => {
+    const articles = await Article.find();
+    res.status(200).send(articles);
 };
 
-exports.getArticles = (req, res) => {
-    const title = req.query.title;
-    const condition = title ? { title: { $regex: new RegExp(title), $options: 'i' } } : {};
-
-    Article.find(condition).then((data) => {
-        console.log("data get", data)
-        res.send(data)
-    }).catch((err) => {
-        res.status(500).send({
-            message:
-                err.message || "Some error occurred while creating the Article."
-        });
+exports.updateArticle = async (req, res) => {
+    const article = await Article.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
     });
-};
-
-exports.updateArticle = (req, res) => {
-    if (!req.body) {
-        return res.status(400).send({
-            message: "Data to update can not be empty!"
-        });
-    }
-    const id = req.params.id;
-    Article.findByIdAndUpdate(id, req.body, { useFindAndModify: false }).then((data) => {
-        if (!data) {
-            res.status(500).send({
-                message: `Cannot update Article with id=${id}. Maybe Article was not found!`
-            });
-        }
-        else res.send({
-            message: "Article was updated successfully."
-        })
-    }).catch(err => {
-        res.status(500).send({
-            message: "Error updating Tutorial with id=" + id
-        });
-    });
+    res.status(200).send(article);
 
 };
 
-exports.deleteArticle = (req, res) => {
-    const id = req.params.id;
-    Article.findByIdAndRemove(id, { useFindAndModify: false }).then((data) => {
-        if (!data) {
-            res.status(404).send({
-                message: `Cannot update Article with id=${id}. Maybe Article was not found!`
-            });
-        }
-        else res.send({
-            message: "Article was deleted successfully."
-        })
-    }).catch(err => {
-        res.status(500).send({
-            message: "Error updating Tutorial with id=" + id
-        });
-    });
+exports.deleteArticle = async (req, res) => {
+    const article = await Article.findByIdAndDelete(req.params.id);
+    res.send(article);
 };
